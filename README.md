e3f73c53ab58d7c6452d9c602e2712ef20b70e28
Jesaja-Schlotzer <jesaja@schlotzer.de>
2024-01-03 16:24:41 +0100

    Initial commit + Java code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OO_Java_vs_Go
+
+Evaluation of the differences of OOP-paradigms of Java and Go.
---
+++ java/src/PassengerTrain.java
@@ -0,0 +1,18 @@
+public class PassengerTrain extends Train{
+
+    int passengerCount;
+
+    public PassengerTrain(int passengerCount) {
+        this.passengerCount = passengerCount;
+    }
+
+    @Override
+    public void refuel(float liter) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
+}
